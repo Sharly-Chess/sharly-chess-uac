aa8e515fb1c4b6a56986de2fb45ea8d2dfc1f354
pascalaubry <66429491+pascalaubry@users.noreply.github.com>
2025-11-26 18:54:46 +0100

    Fixed zip internal path

--- scripts/export/export.py
+++ scripts/export/export.py
@@ -35,7 +35,7 @@ def shutil_delete_onerror(func, path, exc_info):
     func(path)
 
 
-def _delete_folder(
+def delete_folder(
     folder: Path,
 ):
     if folder.is_dir():
@@ -43,7 +43,7 @@ def _delete_folder(
         shutil.rmtree(folder, onerror=shutil_delete_onerror)
 
 
-def _delete_file(
+def delete_file(
     file: Path,
 ):
     if file.is_file():
@@ -54,14 +54,15 @@ def _delete_file(
 basename: str = f'sharly-chess-uac-{SHARLY_CHESS_UAC_VERSION}'
 
 build_dir: Path = BASE_DIR / 'build'
-_delete_folder(build_dir)
+delete_folder(build_dir)
 dist_dir: Path = BASE_DIR / 'dist'
-_delete_folder(dist_dir)
+delete_folder(dist_dir)
+project_dir: Path = dist_dir / basename
 exe_file: Path = dist_dir / f'{basename}.exe'
-_delete_file(exe_file)
+delete_file(exe_file)
 export_dir: Path = BASE_DIR / 'export'
 zip_file: Path = export_dir / f'{basename}.zip'
-_delete_file(zip_file)
+delete_file(zip_file)
 
 print(f'Creating executable [{exe_file}]...')
 run(
@@ -69,12 +70,12 @@ run(
         '--clean',
         '--onefile',
         '--noconfirm',
+        f'--distpath={project_dir}',
         f'--name={basename}',
         '--copy-metadata',
         'sharly_chess_uac',
         '--paths=.',
-        '--optimize',
-        '1',
+        '--optimize=1',
         'src/sharly_chess_uac.py',
         '--windowed',
         '--icon=src/sharly-chess.ico',
@@ -86,10 +87,15 @@ zip_file.parent.mkdir(parents=True, exist_ok=True)
 with ZipFile(zip_file, 'w', ZIP_DEFLATED) as zf:
     cwd: str = os.getcwd()
     os.chdir(dist_dir)
-    zf.write(exe_file.name)
+    for folder_name, sub_folders, file_names in os.walk('.'):
+        zf.write(folder_name, folder_name)
+    for folder_name, sub_folders, file_names in os.walk('.'):
+        for filename in file_names:
+            file_path: Path = Path(folder_name, filename)
+            zf.write(file_path, file_path)
     os.chdir(cwd)
 
-_delete_folder(build_dir)
-_delete_folder(dist_dir)
+# delete_folder(build_dir)
+# delete_folder(dist_dir)
 
 print('Done.')
